b508e72dc09a13891a2bc5d0828aacda348d5a62
EpicBoss32 <72610853+EpicBoss42@users.noreply.github.com>
2022-11-10 10:22:37 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,25 +1,3 @@
-addLayer("e", {})
-addLayer("v", {})
-addLayer("ae", {})
-addLayer("ti", {})
-addLayer("sp", {})
-addLayer("w", {startData() {
-    return {
-        unlocked: false,
-        points: new Decimal(0),
-        tpoints: new Decimal(0),
-        tSec: new Decimal(0),
-        copper: new Decimal(0),
-        copperSec: new Decimal(0),
-    }
-},})
-addLayer("s", {})
-addLayer("si", {})
-addLayer("c", {})
-addLayer("g", {})
-addLayer("m", {})
-addLayer("b", {})
-addLayer("a", {})
-addLayer("ba", {})
-addLayer("bs", {})
-addLayer("t", {})
+addLayer("e", {
+    
+})
